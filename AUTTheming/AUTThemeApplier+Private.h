ae77acd83c4f6c0025b369af8403935f6c1e0e9c
Eric Horacek <horacek.eric@gmail.com>
2015-02-09 10:27:39 -0800

    Update applier private docs

--- AUTTheming/AUTThemeApplier+Private.h
+++ AUTTheming/AUTThemeApplier+Private.h
@@ -15,7 +15,7 @@
 /**
  The applicants of the theme classes, keyed by the the theme class names that they had applied to them.
  
- The values of this dictionary are instances of NSHashTable, containing weak references to all of the objects that have had a class applied to it. When the theme is changed, an applier can apply the new theme to all of its objects.
+ The values of this dictionary are instances of NSHashTable, containing weak references to all of the objects that have had a class applied to it. When the theme property is changed, this enables an applier instance to apply the new theme to all of its previous applicants.
  */
 @property (nonatomic) NSMutableDictionary *applicants;
 
