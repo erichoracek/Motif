f839e94096bf3b6cf1090db360d734180c902924
Fabian Canas <fabian@mojotech.com>
2015-04-09 10:49:58 -0400

    Fix CocoaPod targets

--- Pods/Target Support Files/Pods-Screen Brightness Theming Example/Pods-Screen Brightness Theming Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Screen_Brightness_Theming_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Screen_Brightness_Theming_Example
-@end
