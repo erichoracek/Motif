a433038b8187aa93b3ee2e13076e9c7f86ff342c
Eric Horacek <horacek.eric@gmail.com>
2015-04-09 09:21:35 -0700

    Add files from pod install

---
+++ Pods/Target Support Files/Pods-ScreenBrightnessThemingExample-Masonry/Pods-ScreenBrightnessThemingExample-Masonry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScreenBrightnessThemingExample_Masonry : NSObject
+@end
+@implementation PodsDummy_Pods_ScreenBrightnessThemingExample_Masonry
+@end
---
+++ Pods/Target Support Files/Pods-ScreenBrightnessThemingExample/Pods-ScreenBrightnessThemingExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScreenBrightnessThemingExample : NSObject
+@end
+@implementation PodsDummy_Pods_ScreenBrightnessThemingExample
+@end
